1d8b570477e3c0baee68c8a9314ca92a83ccaa93
Qianglong Mo <sammok2003@hotmail.com>
2014-12-29 17:24:58 +0800

    Create state for Pokemon.

--- app/pokemon/pokemon.js
+++ app/pokemon/pokemon.js
@@ -2,4 +2,20 @@
 
 angular.module('pokemon', [
     'justdex.models.pokemon'
-]);
+])
+    .config(function ($stateProvider){
+        $stateProvider
+            .state('justdex.pokemon', {
+                url: 'pokemon/:pokemon',
+                views: {
+                    'pokemon@': {
+                        templateUrl: 'app/pokemon/pokemon.tmpl.html',
+                        controller: 'PokemonController as PokemonCtrl'
+                    }
+                }
+            })
+    })
+
+    .controller('PokemonController', function (){
+        var PokemonCtrl = this;
+    });
