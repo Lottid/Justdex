aa691a58296058b68bec2852177349b4c41be5a2
Qianglong Mo <sammok2003@hotmail.com>
2014-12-30 17:50:02 +0800

    Create PokemonModel.  Draw pokemon stats bar.

--- app/common/models/pokemon-model.js
+++ app/common/models/pokemon-model.js
@@ -1,3 +1,128 @@
 'use strict';
 
-angular.module('justdex.models.pokemon', []);
+angular.module('justdex.models.pokemon', [])
+    .service('PokemonModel', function ($http){
+        var model = this;
+
+        // get detail;
+        model.getPokemonDetail = function (pokemonName){
+            // get pokemon id then fetch pokemon data
+            return $http.get('data/data.json').then(function (result) {
+                var POKEMONS = result.data,
+                    id;
+
+                // get id
+                _.find(POKEMONS, function (item) {
+                    if (item.name === pokemonName.toLowerCase()) {
+                        id = item.id;
+                        return id || null;
+                    }
+                });
+
+                // fetch pokemon
+                return id ? $http.get('http://pokeapi.co/api/v1/pokemon/' + id).then(function (result) {
+                    return result.data;
+                }) : null;
+            });
+        };
+
+        // get name
+        model.getPokemonName = function (pokemon){
+            return pokemon.name;
+        };
+
+        // get string id
+        model.getPokemonId = function (pokemon){
+            var id = pokemon.pkdx_id;
+
+            if (id < 10){
+                id = '00' + id;
+            } else if (id < 100) {
+                id = '0' + id
+            }
+
+            return id;
+        };
+
+        // get avatar
+        model.getPokemonAvatar = function (pokemon){
+            var id = pokemon.pkdx_id;
+
+            if (id < 10){
+                id = '00' + id;
+            } else if (id < 100) {
+                id = '0' + id
+            }
+
+            return id;
+        };
+
+        // get types
+        model.getPokemonTypes = function (pokemon){
+            return pokemon.types;
+        };
+
+
+        // get height
+        model.getPokemonHeight = function (pokemon){
+            return pokemon.height;
+        };
+
+        // get weight
+        model.getPokemonWeight = function (pokemon){
+            return pokemon.weight;
+        };
+
+        // get abilities
+        model.getPokemonAbilities = function (pokemon){
+            return pokemon.abilities;
+        };
+
+        // get egg groups
+        model.getPokemonEggGroups = function (pokemon){
+            return pokemon.egg_groups;
+        };
+
+        // ============================================================
+        //                       basic stats
+        // ============================================================
+        // get hp
+        model.getPokemonHp = function (pokemon){
+            return pokemon.hp;
+        };
+
+        // get attack
+        model.getPokemonAttack = function (pokemon){
+            return pokemon.attack;
+        };
+
+        // get defense
+        model.getPokemonDefense = function (pokemon){
+            return pokemon.defense;
+        };
+
+        // get special attack
+        model.getPokemonSpAtk = function (pokemon){
+            return pokemon.sp_atk;
+        };
+
+        // get special defense
+        model.getPokemonSpDef = function (pokemon){
+            return pokemon.sp_def;
+        };
+
+        // get speed
+        model.getPokemonSpeed = function (pokemon){
+            return pokemon.speed;
+        };
+
+        // get total
+        model.getPokemonTotal = function (pokemon){
+            return pokemon.total;
+        };
+
+        // get egg cycles
+        model.getPokemonEggCycles = function (pokemon){
+            return pokemon.egg_cycles;
+        };
+    });
--- app/pokemon/pokemon.js
+++ app/pokemon/pokemon.js
@@ -16,6 +16,108 @@ angular.module('pokemon', [
             })
     })
 
-    .controller('PokemonController', function (){
+    .controller('PokemonController', function ($state, $stateParams, PokemonModel){
         var PokemonCtrl = this;
+
+        PokemonModel.pokemon = null;
+
+        PokemonModel.getPokemonDetail($stateParams.pokemon).then(function (result){
+            // check data is successfully fetched;
+            PokemonCtrl.pokemon = result;
+            var pokemon = PokemonCtrl.pokemon;
+
+            if (PokemonCtrl.pokemon === null){
+                $state.go('justdex.list', {});
+            } else {
+                console.log(PokemonCtrl.pokemon);
+                // ============================================================
+                //                     pokemon query method
+                // ============================================================
+                PokemonCtrl.getPokemonName = PokemonModel.getPokemonName;
+                PokemonCtrl.getPokemonId = PokemonModel.getPokemonId;
+                PokemonCtrl.getPokemonAvatar = PokemonModel.getPokemonAvatar;
+                PokemonCtrl.getPokemonHeight = PokemonModel.getPokemonHeight;
+                PokemonCtrl.getPokemonWeight = PokemonModel.getPokemonWeight;
+                PokemonCtrl.getPokemonAbilities = PokemonModel.getPokemonAbilities;
+                PokemonCtrl.getPokemonEggGroups = PokemonModel.getPokemonEggGroups;
+                PokemonCtrl.getPokemonEggCycles = PokemonModel.getPokemonEggCycles;
+                PokemonCtrl.getPokemonTypes = PokemonModel.getPokemonTypes;
+
+                // basic stats
+                PokemonCtrl.getPokemonAttack = PokemonModel.getPokemonAttack;
+                PokemonCtrl.getPokemonHp = PokemonModel.getPokemonHp;
+                PokemonCtrl.getPokemonDefense = PokemonModel.getPokemonDefense;
+                PokemonCtrl.getPokemonSpAtk = PokemonModel.getPokemonSpAtk;
+                PokemonCtrl.getPokemonSpDef = PokemonModel.getPokemonSpDef;
+                PokemonCtrl.getPokemonSpeed = PokemonModel.getPokemonSpeed;
+                PokemonCtrl.getPokemonTotal = PokemonModel.getPokemonTotal;
+
+                // ============================================================
+                //                     get pokemon detail
+                // ============================================================
+
+                // get basic
+                PokemonCtrl.pokemonName = PokemonCtrl.getPokemonName(pokemon);
+                PokemonCtrl.pokemonId = PokemonCtrl.getPokemonId(pokemon);
+                PokemonCtrl.pokemonAvatar = PokemonCtrl.getPokemonAvatar(pokemon);
+                PokemonCtrl.pokemonHeight = PokemonCtrl.getPokemonHeight(pokemon);
+                PokemonCtrl.pokemonWeight = PokemonCtrl.getPokemonWeight(pokemon);
+                PokemonCtrl.pokemonAbilities = PokemonCtrl.getPokemonAbilities(pokemon);
+                PokemonCtrl.pokemonEggGroups= PokemonCtrl.getPokemonEggGroups(pokemon);
+                PokemonCtrl.pokemonEggCycles = PokemonCtrl.getPokemonEggCycles(pokemon);
+                PokemonCtrl.pokemonTypes = PokemonCtrl.getPokemonTypes(pokemon);
+
+                // get basic stats
+                PokemonCtrl.pokemonAttack  = PokemonCtrl.getPokemonAttack(pokemon);
+                PokemonCtrl.pokemonHp = PokemonCtrl.getPokemonHp(pokemon);
+                PokemonCtrl.pokemonDefense = PokemonCtrl.getPokemonDefense(pokemon);
+                PokemonCtrl.pokemonSpAtk = PokemonCtrl.getPokemonSpAtk(pokemon);
+                PokemonCtrl.pokemonSpDef= PokemonCtrl.getPokemonSpDef(pokemon);
+                PokemonCtrl.pokemonSpeed = PokemonCtrl.getPokemonSpeed(pokemon);
+                PokemonCtrl.pokemonTotal = PokemonCtrl.getPokemonTotal(pokemon);
+
+                // draw chat
+                PokemonCtrl.drawChat = function (basic, total){
+                    var percentage,
+                        basic = parseInt(basic),
+                        total = parseInt(total),
+                        color = 'darkred',
+                        isHigherThanNormal = false;
+
+                    if (basic > total) {
+                        percentage = 100;
+                        isHigherThanNormal = true;
+                    } else {
+                        percentage = Math.round((basic/total)*100);
+                    }
+
+                    // sett color
+                    if (percentage <= 10) {
+                        color = 'darkred';
+                    } else if (percentage <= 25) {
+                        color = 'red';
+                    } else if (percentage <= 50) {
+                        color = 'yellow';
+                    } else if (percentage <= 75) {
+                        color = 'orange';
+                    } else if (percentage <= 100) {
+                        color = 'darkorange';
+                        if (isHigherThanNormal) {
+                            color = 'red';
+                        }
+                    }
+
+                    return {percentage: percentage, color: color};
+                };
+
+                PokemonCtrl.drawChatHp = PokemonCtrl.drawChat(PokemonCtrl.pokemonHp, 200);
+                PokemonCtrl.drawChatAttack = PokemonCtrl.drawChat(PokemonCtrl.pokemonAttack, 180);
+                PokemonCtrl.drawChatDefense = PokemonCtrl.drawChat(PokemonCtrl.pokemonDefense, 180);
+                PokemonCtrl.drawChatSpAtk = PokemonCtrl.drawChat(PokemonCtrl.pokemonSpAtk, 180);
+                PokemonCtrl.drawChatSpDef = PokemonCtrl.drawChat(PokemonCtrl.pokemonSpDef, 180);
+                PokemonCtrl.drawChatSpeed = PokemonCtrl.drawChat(PokemonCtrl.pokemonSpeed, 180);
+            }
+        });
+
+
     });
